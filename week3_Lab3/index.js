6b9bbb59720a6a2a49e24363e8591e6fcf79dbc6
almas1990 <almas.sultana@georgebrown.ca>
2026-09-24 19:09:22 -0400

    Add response headers to Week 3 Lab

--- week3_Lab3/index.js
+++ week3_Lab3/index.js
@@ -13,19 +13,23 @@ const server = http.createServer((req, res) => {
         res.end(`{"error": "${http.STATUS_CODES[405]}"}`)
     } else {
         if (req.url === '/') {
+            res.setHeader("Content-Type", "text/html");
             res.end("<h1>Welcome to Lab Exercise 03</h1>");
         }
 
         else if (req.url === '/employee')  {
+            res.setHeader("Content-Type", "application/json");
             res.end(JSON.stringify(employees));
         }
 
         else if (req.url === '/employee/names')  {
+            res.setHeader("Content-Type", "application/json");
            const names = employees.map(emp => `${emp.firstName} ${emp.lastName}`).sort();
            res.end(JSON.stringify(names));
         }
 
         else if (req.url === '/employee/totalsalary') {
+            res.setHeader("Content-Type", "application/json");
             const totalSalary = employees.reduce((total, emp) => total + emp.Salary, 0);
             res.end(JSON.stringify({ total_salary: totalSalary }));
     }
